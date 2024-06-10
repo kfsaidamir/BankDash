2781606783ac8ff72954a0c618482a50c45822f0
kf_saidamir <skadirov2009@gmail.com>
2024-06-10 14:14:56 +0500

    A

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
     <ChakraProvider>
-      <HashRouter>
+      <BrowserRouter>
       <App />
-      </HashRouter>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,  
 )
